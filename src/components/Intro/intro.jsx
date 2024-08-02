ea24d80b49a57d848b1b9a7dc2f6247ad084f2ab
Varun K S <90751170+varun18072001@users.noreply.github.com>
2024-08-02 12:44:24 +0530

    second commit

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './intro.css'
 import bg from '../../assets/image.png'
 import { Link } from 'react-scroll';
-import btnImg from '../../assets/hireme.jpg';
+import resumeImg from '../../assets/resume.jpg';
+import resumePdf from '../../assets/Resume.pdf'
 
 const Intro = () => {
   return (
@@ -11,11 +12,11 @@ const Intro = () => {
         <span className="hello">Hello,<br /></span>
         <span className="introText">I am <span className='introName'>Varun K S</span><br />Web Developer</span>
         <p className='introPara'>I’m an aspiring web developer with a strong foundation in creating<br />visually appealing and user-friendly websites.</p>
-        <Link><button className="btn"><img src={btnImg} alt="Resume" className='btnImg'/>Resume</button></Link>
+        <a href={resumePdf} target='_blank' rel='noopener noreferrer'><button className="btn"><img src={resumeImg} alt="Resume" className='btnImg'/>Resume</button></a>
       </div>
       <img src={bg} alt="Profile" className='bg' />
     </section>
   )
 }
 
-export default Intro
+export default Intro;
